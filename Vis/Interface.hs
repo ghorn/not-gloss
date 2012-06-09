de9cbd83af7cb000c5f3c7077da632fdf810c073
Greg Horn <gregmainland@gmail.com>
2012-06-09 14:30:38 -0700

    use where instead of let

--- Vis/Interface.hs
+++ Vis/Interface.hs
@@ -21,16 +21,16 @@ animate userDrawFun = animateIO (return . userDrawFun)
 
 -- | display an animation
 animateIO :: Real b => (Float -> IO (VisObject b)) -> IO ()
-animateIO userDrawFun = do
-  let ts = 0.01
-      state0 = ()
-      drawFun _ (_,time) = userDrawFun time
-      simFun _ _ = return ()
-      camera0 = Camera0 { phi0 = 60
-                        , theta0 = 20
-                        , rho0 = 7}
-
-  vis camera0 simFun drawFun state0 ts
+animateIO userDrawFun = vis camera0 simFun drawFun state0 ts
+  where
+    ts = 0.01
+    state0 = ()
+    drawFun _ (_,time) = userDrawFun time
+    simFun _ _ = return ()
+    camera0 = Camera0 { phi0 = 60
+                      , theta0 = 20
+                      , rho0 = 7}
+  
 
 -- | run a simulation
 simulate :: Real b => Double -> world -> (world -> VisObject b) -> (Float -> world -> world) -> IO ()
@@ -39,12 +39,10 @@ simulate ts state0 userDrawFun userSimFun =
 
 -- | run a simulation
 simulateIO :: Real b => Double -> world -> (world -> IO (VisObject b)) -> (Float -> world -> IO world) -> IO ()
-simulateIO ts state0 userDrawFun userSimFun = do
-  let drawFun _ (state,_) = userDrawFun state
-      simFun _ (state,time) = userSimFun time state
-      camera0 = Camera0 { phi0 = 60
-                        , theta0 = 20
-                        , rho0 = 7}
-
-  vis camera0 simFun drawFun state0 ts
-
+simulateIO ts state0 userDrawFun userSimFun = vis camera0 simFun drawFun state0 ts
+  where
+    drawFun _ (state,_) = userDrawFun state
+    simFun _ (state,time) = userSimFun time state
+    camera0 = Camera0 { phi0 = 60
+                      , theta0 = 20
+                      , rho0 = 7}
