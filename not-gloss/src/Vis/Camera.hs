2662fb782ad0ca40185dfbf287e62ec2f8ecba5a
Greg Horn <greghorn@google.com>
2015-07-20 16:20:25 -0700

    add middle button z translation

--- not-gloss/src/Vis/Camera.hs
+++ not-gloss/src/Vis/Camera.hs
@@ -29,6 +29,7 @@ data Camera = Camera { phi :: GLdouble
                      , ballY :: GLint 
                      , leftButton :: GLint
                      , rightButton :: GLint
+                     , middleButton :: GLint
                      }
 
 makeCamera :: Camera0 -> Camera
@@ -40,6 +41,7 @@ makeCamera camera0 = Camera { phi   = phi0 camera0
                             , ballY = (-1)
                             , leftButton = 0
                             , rightButton = 0
+                            , middleButton = 0
                             }
 
 setCamera :: Camera -> IO ()
@@ -55,13 +57,16 @@ setCamera camera = GLUT.lookAt (Vertex3 xc yc zc) (Vertex3 x0 y0 z0) (Vector3 0 
     zc = z0 - rho'*sin(theta'*pi/180)
 
 cameraMotion :: Camera -> Position -> Camera
-cameraMotion (Camera phi0' theta0' rho0' (V3 x0 y0 z0) bx by lb rb) (Position x y) =
-  Camera nextPhi nextTheta rho0' nextPos nextBallX nextBallY lb rb
+cameraMotion (Camera phi0' theta0' rho0' (V3 x0 y0 z0) bx by lb rb mb) (Position x y) =
+  Camera nextPhi nextTheta rho0' nextPos nextBallX nextBallY lb rb mb
   where
     deltaX
       | bx == -1  = 0
       | otherwise = fromIntegral (x - bx)
     deltaY
+      | by == -1  = 0
+      | otherwise = fromIntegral (y - by)
+    deltaZ
       | by == -1  = 0
       | otherwise = fromIntegral (y - by)
     nextTheta'
@@ -70,21 +75,23 @@ cameraMotion (Camera phi0' theta0' rho0' (V3 x0 y0 z0) bx by lb rb) (Position x 
       | otherwise              = deltaY + theta0'
     nextX = x0 + 0.003*rho0'*( -sin(phi0'*pi/180)*deltaX - cos(phi0'*pi/180)*deltaY)
     nextY = y0 + 0.003*rho0'*(  cos(phi0'*pi/180)*deltaX - sin(phi0'*pi/180)*deltaY)
+    nextZ = z0 - 0.001*rho0'*deltaZ
 
     (nextPhi, nextTheta) = if lb == 1
                            then (phi0' + deltaX, nextTheta')
                            else (phi0', theta0')
 
-    nextPos = if rb == 1
-              then V3 nextX nextY z0
-              else V3 x0 y0 z0
+    nextPos
+      | rb == 1 = V3 nextX nextY z0
+      | mb == 1 = V3 x0 y0 nextZ
+      | otherwise = V3 x0 y0 z0
 
     nextBallX = x
     nextBallY = y
 
 cameraKeyboardMouse :: Camera -> Key -> KeyState -> Camera
 cameraKeyboardMouse camera key keyState =
-  camera {rho = newRho, leftButton = lb, rightButton = rb, ballX = bx, ballY = by}
+  camera {rho = newRho, leftButton = lb, rightButton = rb, middleButton = mb, ballX = bx, ballY = by}
   where
     (lb, reset0) = case (key, keyState) of (MouseButton LeftButton, Down) -> (1, True)
                                            (MouseButton LeftButton, Up) -> (0, False)
@@ -92,8 +99,11 @@ cameraKeyboardMouse camera key keyState =
     (rb, reset1) = case (key, keyState) of (MouseButton RightButton, Down) -> (1, True)
                                            (MouseButton RightButton, Up) -> (0, False)
                                            _ -> (rightButton camera, False)
+    (mb, reset2) = case (key, keyState) of (MouseButton MiddleButton, Down) -> (1, True)
+                                           (MouseButton MiddleButton, Up) -> (0, False)
+                                           _ -> (middleButton camera, False)
   
-    (bx,by) = if reset0 || reset1 then (-1,-1) else (ballX camera, ballY camera)
+    (bx,by) = if reset0 || reset1 || reset2 then (-1,-1) else (ballX camera, ballY camera)
   
     newRho = case (key, keyState) of (MouseButton WheelUp, Down)   -> 0.9 * (rho camera)
                                      (MouseButton WheelDown, Down) -> 1.1 * (rho camera)
